799f85b42703faeafda232458454562b128689bb
Alvaro Gamez Machado <alvaro.gamez@hazent.com>
2018-06-15 10:25:05 +0200

    Add libHello source
    This simple library only have two entries: the standard hello()
    which prints a salute and one called fmod_wrapper, which simply
    calls fmod(x, y), that we will use to test autotools linkage against
    a library.
    
    Include also a template for generating libHello.pc, so our library
    can be easily used via means of pkg-config

---
+++ libs/libHello.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+double fmod_wrapper(double x, double y)
+{
+	return fmod(x, y);
+}
+
+int hello()
+{
+	printf("Hello world!\n");
+}
---
+++ libs/libHello.h
@@ -0,0 +1,7 @@
+#ifndef __libHello_h__
+#define __libHello_h__
+
+double fmod_wrapper(double x, double y);
+int hello();
+
+#endif /* __libHello_h__ */
